231d725fd70376ebe8284e8cc5f25d0915018c31
Victor Blaga <victor.blaga@revodata.nl>
2024-03-05 17:07:50 +0100

    Added REST API as a data source

--- pyspark_datasources/__init__.py
+++ pyspark_datasources/__init__.py
@@ -1,3 +1,6 @@
 from .fake import FakeDataSource
 from .github import GithubDataSource
 from .huggingface import HuggingFaceDatasets
+from .restapi import RestapiDataSource
+
+__all__ = ["FakeDataSource", "GithubDataSource", "HuggingFaceDatasets", "RestapiDataSource"]
