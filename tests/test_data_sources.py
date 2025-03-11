7924e47d626da47105dbfb1e21eb8df23152ba8d
Haoyu Weng <wenghy02@gmail.com>
2025-03-11 16:39:36 -0700

    Add fake streaming source

--- tests/test_data_sources.py
+++ tests/test_data_sources.py
@@ -17,6 +17,21 @@ def test_github_datasource(spark):
     assert len(prs) > 0
 
 
+def test_fake_datasource_stream(spark):
+    spark.dataSource.register(FakeDataSource)
+    (
+        spark.readStream.format("fake")
+        .load()
+        .writeStream.format("memory")
+        .queryName("result")
+        .trigger(once=True)
+        .start()
+        .awaitTermination()
+    )
+    spark.sql("SELECT * FROM result").show()
+    assert spark.sql("SELECT * FROM result").count() == 3
+
+
 def test_fake_datasource(spark):
     spark.dataSource.register(FakeDataSource)
     df = spark.read.format("fake").load()
