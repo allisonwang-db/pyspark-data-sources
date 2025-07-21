53d4427be69da896c33efbf8656f6b30cf5578cb
Allison Wang <allison.wang@databricks.com>
2025-07-21 14:26:33 -0700

    update

--- pyspark_datasources/__init__.py
+++ pyspark_datasources/__init__.py
@@ -3,5 +3,6 @@ from .github import GithubDataSource
 from .googlesheets import GoogleSheetsDataSource
 from .huggingface import HuggingFaceDatasets
 from .kaggle import KaggleDataSource
+from .opensky import OpenSkyDataSource
 from .simplejson import SimpleJsonDataSource
 from .stock import StockDataSource
